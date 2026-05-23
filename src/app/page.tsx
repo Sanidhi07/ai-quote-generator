4d6e0ac06d2073f7438a6b97ba903ab2f70c9a1b
= <Sanidhi2207@gmail.com>
2026-05-23 15:11:19 +0530

    feat: add tone selector and finalize generate button logic

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -4,7 +4,8 @@ import { useState } from "react";
 export default function Home(){
 const [result, setResult]=useState("");
 const [topic,setTopic]=useState("")
-
+const [category,setCategory]=useState("Coding");
+const [tone,setTone]=useState("Inspirational")
 
 const testAI=async()=>{
 
@@ -30,13 +31,43 @@ const testAI=async()=>{
 return(
   <main className="flex min-h-screen flex-col items-center justify-center p-24">
       <h1 className="text-3xl font-bold">AI Quote Generator</h1>
-      {/* Topic Input */}
+      {/* --- Topic Input --- */}
       <input type="text"
       placeholder="Enter a topic (e.g Coding)"
       value={topic}
       onChange={(e) => setTopic(e.target.value)}
       className="mt-6 p-2 border border-gray-300 rounded text-white w-64 text-center"
       />
+
+      {/* --- Category Selector --- */}
+      <div className="flex flex-col gap-2 mt-4">
+        <label className="text-sm font-semibold text-white">Select Category:</label>
+        <select
+          value={category}
+          onChange={(e) => setCategory(e.target.value)}
+          className="p-2 border border-gray-300 rounded bg-black text-shadow-white w-64 focus:ring-2 focus:ring-blue-500 outline-none"
+        >
+          <option value="Coding">Coding</option>
+          <option value="Motivation">Motivation</option>
+          <option value="Philosophy">Philosophy</option>
+          <option value="Life">Life</option>
+        </select>
+
+      </div>
+      {/* --- Tone Selector --- */}
+      <div className="flex flex-col gap-2 mt-4">
+        <label className="text-sm font-semibold text-white">Select Tone:</label>
+        <select
+          value={tone}
+          onChange={(e) => setTone(e.target.value)}
+          className="p-2 border border-gray-300 rounded bg-black text-shadow-white w-64 focus:ring-2 focus:ring-blue-500 outline-none"
+        >
+          <option value="Inspirational">Inspirational</option>
+          <option value="Motivational">Motivational</option>
+          <option value="Philosophical">Philosophical</option>
+          <option value="Humorous">Humorous</option>
+        </select>
+      </div>
 
       <button 
         onClick={testAI}
